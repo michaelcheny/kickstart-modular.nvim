9ce0174f37546e93faa7c40e84b4efe6f5533339
Michael Chen <michaelchengaming@gmail.com>
2024-05-21 20:53:36 -0700

    maybe ill stick with dracula theme?

--- lua/kickstart/plugins/colorscheme.lua
+++ lua/kickstart/plugins/colorscheme.lua
@@ -1,48 +1,58 @@
 return {
   {
-    'catppuccin/nvim',
+    'dracula/vim',
     lazy = false,
     priority = 1000,
     config = function()
-      require('catppuccin').setup {
-        integrations = {
-          aerial = true,
-          alpha = true,
-          cmp = true,
-          dashboard = true,
-          flash = true,
-          gitsigns = true,
-          headlines = true,
-          illuminate = true,
-          indent_blankline = { enabled = true },
-          leap = true,
-          lsp_trouble = true,
-          mason = true,
-          markdown = true,
-          mini = true,
-          native_lsp = {
-            enabled = true,
-            underlines = {
-              errors = { 'undercurl' },
-              hints = { 'undercurl' },
-              warnings = { 'undercurl' },
-              information = { 'undercurl' },
-            },
-          },
-          navic = { enabled = true, custom_bg = 'lualine' },
-          neotest = true,
-          neotree = true,
-          noice = true,
-          notify = true,
-          semantic_tokens = true,
-          telescope = true,
-          treesitter = true,
-          treesitter_context = true,
-          which_key = true,
-        },
-      }
-      vim.cmd.colorscheme 'catppuccin-macchiato'
+      vim.cmd.colorscheme 'dracula'
     end,
   },
 }
+-- return {
+--   {
+--     'catppuccin/nvim',
+--     lazy = false,
+--     priority = 1000,
+--     config = function()
+--       require('catppuccin').setup {
+--         integrations = {
+--           aerial = true,
+--           alpha = true,
+--           cmp = true,
+--           dashboard = true,
+--           flash = true,
+--           gitsigns = true,
+--           headlines = true,
+--           illuminate = true,
+--           indent_blankline = { enabled = true },
+--           leap = true,
+--           lsp_trouble = true,
+--           mason = true,
+--           markdown = true,
+--           mini = true,
+--           native_lsp = {
+--             enabled = true,
+--             underlines = {
+--               errors = { 'undercurl' },
+--               hints = { 'undercurl' },
+--               warnings = { 'undercurl' },
+--               information = { 'undercurl' },
+--             },
+--           },
+--           navic = { enabled = true, custom_bg = 'lualine' },
+--           neotest = true,
+--           neotree = true,
+--           noice = true,
+--           notify = true,
+--           semantic_tokens = true,
+--           telescope = true,
+--           treesitter = true,
+--           treesitter_context = true,
+--           which_key = true,
+--         },
+--       }
+--       vim.cmd.colorscheme 'catppuccin-macchiato'
+--     end,
+--   },
+-- }
 -- vim: ts=2 sts=2 sw=2 et
