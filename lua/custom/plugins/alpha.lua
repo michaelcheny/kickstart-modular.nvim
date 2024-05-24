a40a6d1aa40e4f133e7d6493cf3c449ed7eac892
Michael Chen <michaelchengaming@gmail.com>
2024-05-24 16:20:03 -0700

    add a dashboard

---
+++ lua/custom/plugins/alpha.lua
@@ -0,0 +1,55 @@
+return {
+  'goolord/alpha-nvim',
+  event = 'VimEnter',
+  config = function()
+    local alpha = require 'alpha'
+    local dashboard = require 'alpha.themes.dashboard'
+    -- local fortune = require 'alpha.fortune'
+
+    -- Set header
+    dashboard.section.header.val = {
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '⠄⠄⠄⠄⠄⠄⠄⢀⣠⣶⣾⣿⣶⣦⣤⣀⠄⢀⣀⣤⣤⣤⣤⣄⠄⠄⠄⠄⠄⠄',
+      '⠄⠄⠄⠄⠄⢀⣴⣿⣿⣿⡿⠿⠿⠿⠿⢿⣷⡹⣿⣿⣿⣿⣿⣿⣷⠄⠄⠄⠄⠄',
+      '⠄⠄⠄⠄⠄⣾⣿⣿⣿⣯⣵⣾⣿⣿⡶⠦⠭⢁⠩⢭⣭⣵⣶⣶⡬⣄⣀⡀⠄⠄',
+      '⠄⠄⠄⡀⠘⠻⣿⣿⣿⣿⡿⠟⠩⠶⠚⠻⠟⠳⢶⣮⢫⣥⠶⠒⠒⠒⠒⠆⠐⠒',
+      '⠄⢠⣾⢇⣿⣿⣶⣦⢠⠰⡕⢤⠆⠄⠰⢠⢠⠄⠰⢠⠠⠄⡀⠄⢊⢯⠄⡅⠂⠄',
+      '⢠⣿⣿⣿⣿⣿⣿⣿⣏⠘⢼⠬⠆⠄⢘⠨⢐⠄⢘⠈⣼⡄⠄⠄⡢⡲⠄⠂⠠⠄',
+      '⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣥⣀⡁⠄⠘⠘⠘⢀⣠⣾⣿⢿⣦⣁⠙⠃⠄⠃⠐⣀',
+      '⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣋⣵⣾⣿⣿⣿⣿⣦⣀⣶⣾⣿⣿⡉⠉⠉',
+      '⣿⣿⣿⣿⣿⣿⣿⠟⣫⣥⣬⣭⣛⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡆⠄',
+      '⣿⣿⣿⣿⣿⣿⣿⠸⣿⣏⣙⠿⣿⣿⣶⣦⣍⣙⠿⠿⠿⠿⠿⠿⠿⠿⣛⣩⣶⠄',
+      '⣛⣛⣛⠿⠿⣿⣿⣿⣮⣙⠿⢿⣶⣶⣭⣭⣛⣛⣛⣛⠛⠛⠻⣛⣛⣛⣛⣋⠁⢀',
+      '⣿⣿⣿⣿⣿⣶⣬⢙⡻⠿⠿⣷⣤⣝⣛⣛⣛⣛⣛⣛⣛⣛⠛⠛⣛⣛⠛⣡⣴⣿',
+      '⣛⣛⠛⠛⠛⣛⡑⡿⢻⢻⠲⢆⢹⣿⣿⣿⣿⣿⣿⠿⠿⠟⡴⢻⢋⠻⣟⠈⠿⠿',
+      '⣿⡿⡿⣿⢷⢤⠄⡔⡘⣃⢃⢰⡦⡤⡤⢤⢤⢤⠒⠞⠳⢸⠃⡆⢸⠄⠟⠸⠛⢿',
+      '⡟⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢸',
+    }
+    -- Set menu
+    dashboard.section.buttons.val = {
+      dashboard.button('e', '  > Toggle file explorer', '<cmd>lua require("oil").toggle_float()<cr>'),
+      dashboard.button('sf', '󰱼  > Search File', '<cmd>Telescope find_files<CR>'),
+      dashboard.button('sg', '  > Search Word', '<cmd>Telescope live_grep<CR>'),
+      -- dashboard.button('wr', '󰁯  > Restore Session For CD', '<cmd>SessionRestore<CR>'), need to use https://github.com/rmagatti/auto-session for this
+      dashboard.button('r', '  > Recent', ':Telescope oldfiles<CR>'),
+      dashboard.button('q', '  > GTFO', '<cmd>qa<CR>'),
+    }
+
+    -- dashboard.section.footer.val = fortune()
+    dashboard.section.footer.val = 'hello world'
+
+    -- Send config to alpha
+    alpha.setup(dashboard.opts)
+
+    -- Disable folding on alpha buffer
+    vim.cmd [[autocmd FileType alpha setlocal nofoldenable]]
+  end,
+}
